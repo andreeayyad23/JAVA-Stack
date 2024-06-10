def000da8b534e4eef325859a5f56a5ba5ec986f
Andreh,ayyad <andreh,ayad@gmail.com>
2024-06-10 11:56:46 +0300

    First Project For Java

---
+++ Java Fundamentals/First_Java_Program/First_Java_Program.java
@@ -0,0 +1,7 @@
+public class First_Java_Program {
+    public static void main(String[] args) {
+        System.out.println("My Name Is Andree Ayyad");
+        System.out.println("I am 23 years old!");
+        System.out.println("My hometown is Bethlehem, PS");
+    }
+}
