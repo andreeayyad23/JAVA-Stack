3958f55e4f3420d08ad15bd64c23cb181a08625e
Andreh,ayyad <andreh,ayad@gmail.com>
2024-06-22 22:37:01 +0300

    daikichi

---
+++ Java Spring/daikichi_routes/src/main/java/com/example/DaikichiController/DaikichRoutesApplication.java
@@ -0,0 +1,13 @@
+package com.example.DaikichiController;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaikichRoutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaikichRoutesApplication.class, args);
+	}
+
+}
---
+++ Java Spring/daikichi_routes/src/main/java/com/spring/daikichRoutes/DaikichRoutesApplication.java
@@ -0,0 +1,13 @@
+package com.spring.daikichRoutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaikichRoutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaikichRoutesApplication.class, args);
+	}
+
+}
